df3b10492b7771a534ffb2c040577b68c1cf0031
pjj3yy <bogdan.orczykowski@aptiv.com>
2022-12-14 15:47:43 +0100

    Fill StoreItems using product_list. Added separate config

---
+++ config.py
@@ -0,0 +1,14 @@
+url = "https://www.amazon.fr/promotion/psp/productInfoList"
+
+payload = "promotionId=A2KA67KJQ3R6VT&anti-csrftoken-a2z=hBvEbHoebBSwV0FpblgOc73klc6AKiioe3%2BDRDCdA03ZAAAAAGN6XeEAAAAB"
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Host": "www.amazon.fr",
+    "User-Agent": "HTTPie"
+}
+
+cookies = {
+    "session-id": "258-8434095-1415205",
+    "ubid-acbfr": "260-4128488-8114845",
+}
--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+import re
+
 from pydantic import BaseModel
 
 
@@ -23,3 +25,12 @@ class LegoSet(BaseModel):
 
 class StoreItems(BaseModel):
     items: list[BaseModel] = []
+
+    def from_product_list(self, product_list):
+        """Fill store items from product list."""
+
+        for product in product_list:
+            LS = LegoSet(**product)
+            s = re.search(r"(\d{4,6})", LS.title)
+            LS.setNumber = s.group() if s else "unknown"
+            self.items.append(LS)
